6cc7b38877fc8193de3c9a9ee122101d2b04f2fb
rmcclorey1125 <rmcclorey1@yahoo.com>
2020-07-08 17:03:33 -0400

    News

--- stock_market_watch_frontend/src/script.js
+++ stock_market_watch_frontend/src/script.js
@@ -12,12 +12,38 @@ document.addEventListener("DOMContentLoaded", function(e){
     } else if (e.target.value === "go"){
       
     } else if (e.target.className === "show-info"){
-      console.log(e.target.parentNode.dataset.news)
+      console.log(e.target.parentNode.dataset.news[0])
       const stockDiv = e.target.parentNode
       const stockData = e.target.parentNode.dataset
       const moreInfo = document.createElement('div')
-      const stockNewsUl = document.querySelector(".stock-news")
+      const stockNewsUl = document.createElement('ul')
       moreInfo.className = "more-info"
+
+      newsArray = stockData.news.split("#<IEX::Resources::News ")
+        newsArray.shift()
+        // console.log(newsArray)
+
+        newsArray.forEach(i => {
+
+          i.slice(1, -3)
+
+          let keyVal = i.split("=");
+            // console.log(keyVal[3])
+          obj = { }
+          obj["date"] = keyVal[1].slice(0, -9)
+          obj["headline"] = keyVal[2].slice(1, -1)
+          obj["body"] = keyVal[5].slice(1, -5)
+          obj["src"] = keyVal[6].slice(1, -8)
+
+
+           li = document.createElement('li')
+           li.innerHTML =
+             `<h3>${obj.headline}</h3><br>
+               <p>${obj.body}<br>source: ${obj.src}</p><br>
+              `
+               stockNewsUl.append(li)
+
+        })
       moreInfo.innerHTML = `
         <p>Total volume: ${stockData.avg_total_volume}</p>
         <p>Change percent: ${stockData.change_percent}</p>
@@ -28,8 +54,9 @@ document.addEventListener("DOMContentLoaded", function(e){
         <p>Primary exchange: ${stockData.primary_exchange}</p>
         <p>Symbol: ${stockData.symbol}</p>
         <p>YTD change: ${stockData.ytd_change}</p>
-        <p>News: ${stockData.news}</p>
+        <p>News: </p>
       `
+      moreInfo.append(stockNewsUl)
       stockDiv.append(moreInfo)
       // stockDiv.append(stockNewsUl)
       e.target.className = "hide-info"
@@ -61,8 +88,8 @@ document.addEventListener("DOMContentLoaded", function(e){
     function renderStock(stock){
       const stockDiv = document.createElement('div')
       const stockImg = document.createElement('img')
-      const stockNewsUl = document.createElement('ul')
-      stockNewsUl.className = "stock-news"
+      
+      // stockNewsUl.className = "stock-news"
 
     stockImg.src = stock.logo_url
     stockImg.setAttribute("class", "company_logo")
@@ -80,37 +107,13 @@ document.addEventListener("DOMContentLoaded", function(e){
     stockDiv.dataset.primary_exchange = `${stock.primary_exchange}`
     stockDiv.dataset.symbol = `${stock.symbol}`
     stockDiv.dataset.ytd_change = `${stock.ytd_change}`
-    // stockDiv.dataset.news = `${stockNewsUl}`
+    stockDiv.dataset.news = `${stock.news}`
 
       stockDiv.prepend(stockImg)
 
-        newsArray = stock.news.split("#<IEX::Resources::News ")
-        newsArray.shift()
-        // console.log(newsArray)
-
-        newsArray.forEach(i => {
-
-          i.slice(1, -3)
-
-          let keyVal = i.split("=");
-            console.log(keyVal)
-          obj = { }
-          obj["date"] = keyVal[1].slice(0, -9)
-          obj["headline"] = keyVal[2].slice(1, -1)
-          obj["body"] = keyVal[5].slice(1, -5)
-          obj["src"] = keyVal[6].slice(1, -8)
 
 
-           li = document.createElement('li')
-           li.innerHTML =
-             `<h3>${obj.headline}</h3><br>
-               <p>${obj.body}<br>source: ${obj.src}</p><br>
-              `
-               stockNewsUl.append(li)
-
-        })
-        
-        stockDiv.append(stockNewsUl)
+        // stockDiv.append(stockNewsUl)
         stockCollection.append(stockDiv)
 
 
