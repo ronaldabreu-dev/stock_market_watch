c68cfe4b5d16a3c80350c69a7e569c8c905cb612
ra-bluebirdgroup <ra@bluebirdgroup.ltd>
2020-07-08 17:31:39 -0400

    merged

--- stock_market_watch_frontend/src/script.js
+++ stock_market_watch_frontend/src/script.js
@@ -1,137 +1,171 @@
 document.addEventListener("DOMContentLoaded", function(e){
- const stocksUrl = "http://localhost:3000/api/v1/stocks"
- const stockCollection = document.querySelector("#stock-collection")
- const searchImg = document.createElement('img')
+  const stocksUrl = "http://localhost:3000/api/v1/stocks"
+  const stockCollection = document.querySelector("#stock-collection")
+  const searchImg = document.createElement('img')
 
-document.addEventListener("click", function(e){
+  document.addEventListener("click", function(e){
     e.preventDefault()
     if(e.target.textContent === "On The Move"){
       stockCollection.innerHTML = ""
       getStocks()
-
     } else if (e.target.textContent === "Sign up"){
       console.log("SignUp")
     } else if (e.target.textContent === "Log in"){
 
     } else if (e.target.value === "go"){
-       e.preventDefault()
+      e.preventDefault()
        const searchInput = document.getElementById("search-input")
        getStock(searchInput.value)
+    } else if (e.target.className === "show-info"){
+      console.log(e.target.parentNode.dataset.news[0])
+      const stockDiv = e.target.parentNode
+      const stockData = e.target.parentNode.dataset
+      const moreInfo = document.createElement('div')
+      const stockNewsUl = document.createElement('ul')
+      moreInfo.className = "more-info"
+
+      newsArray = stockData.news.split("#<IEX::Resources::News ")
+        newsArray.shift()
+        // console.log(newsArray)
+
+        newsArray.forEach(i => {
+
+          i.slice(1, -3)
+
+          let keyVal = i.split("=");
+            // console.log(keyVal[3])
+          obj = { }
+          obj["date"] = keyVal[1].slice(0, -9)
+          obj["headline"] = keyVal[2].slice(1, -1)
+          obj["body"] = keyVal[5].slice(1, -5)
+          obj["src"] = keyVal[6].slice(1, -8)
+
+
+           li = document.createElement('li')
+           li.innerHTML =
+             `<h3>${obj.headline}</h3><br>
+               <p>${obj.body}<br>source: ${obj.src}</p><br>
+              `
+               stockNewsUl.append(li)
+
+        })
+      moreInfo.innerHTML = `
+        <p>Total volume: ${stockData.avg_total_volume}</p>
+        <p>Change percent: ${stockData.change_percent}</p>
+        <p>Latest price: ${stockData.latest_price}</p>
+        <p>Latest update: ${stockData.latest_update}</p>
+        <p>Market cap: ${stockData.market_cap}</p>
+        <p>P/E ratio: ${stockData.pe_ratio}</p>
+        <p>Primary exchange: ${stockData.primary_exchange}</p>
+        <p>Symbol: ${stockData.symbol}</p>
+        <p>YTD change: ${stockData.ytd_change}</p>
+        <p>News: </p>
+      `
+      moreInfo.append(stockNewsUl)
+      stockDiv.append(moreInfo)
+      // stockDiv.append(stockNewsUl)
+      e.target.className = "hide-info"
+      e.target.textContent = "Hide Info"
+    } else if (e.target.className === "hide-info"){
+      const moreInfo = document.querySelector(".more-info")
+      moreInfo.remove()
+      e.target.className = "show-info"
+      e.target.textContent = "Show Info"
     }
   })
 
-function getStock(stockSymbol){
-  const stockUrl = `https://cloud.iexapis.com/stable/stock/${stockSymbol}/batch?types=quote,news&range=1m&last=10&token=pk_f57a13c9af324593872971b36ca28c8c`
-  fetch(stockUrl)
-  .then(response => response.json())
-  .then(data => {
-    renderSearch(data)
- })
-}
+    function getStock(stockSymbol){
+      const stockUrl = `https://cloud.iexapis.com/stable/stock/${stockSymbol}/batch?types=quote,news&range=1m&last=10&token=pk_f57a13c9af324593872971b36ca28c8c`
+      fetch(stockUrl)
+      .then(response => response.json())
+      .then(data => {
+        renderSearch(data)
+     })
+    }
 
-function renderSearch(stock){
-  let stockDiv = document.createElement('div')
-  let stockNewsUl = document.createElement('ul')
-      setStockImage(stock.quote.symbol)
-  // searchImg.setAttribute("class", "company_logo")
+    function renderSearch(stock){
+      let stockDiv = document.createElement('div')
+      let stockNewsUl = document.createElement('ul')
+          setStockImage(stock.quote.symbol)
+      // searchImg.setAttribute("class", "company_logo")
 
-     console.log(searchImg)
+         console.log(searchImg)
+        stockDiv.setAttribute("class", "each-stock")
+        stockDiv.innerHTML = `
+        <h1>${stock.quote.companyName}</h1><br>
+        <b>avg total volume:</b> ${stock.quote.avgTotalVolume}<br>
+        <b>change percent:</b> ${stock.quote.changePercent}<br>
+        <b>latest price:</b> ${stock.quote.latestPrice}<br>
+        <b>latest update:</b> ${stock.quote.latestTime}<br>
+        <b>market cap:</b> ${stock.quote.marketCap}<br>
+        <b>pe ratio:</b> ${stock.quote.peRatio}<br>
+        <b>primary exchange:</b> ${stock.quote.primaryExchange}<br>
+        <b>symbol:</b> ${stock.quote.symbol}<br>
+        <b>week52High:</b> ${stock.quote.week52High}<br>
+        <b>week52Low:</b> ${stock.quote.week52Low}<br>
+        <b>ytd change:</b> ${stock.quote.ytdChange}<br>
+        <b>news:</b><br>
+        `
+        stockDiv.prepend(searchImg)
+        stockCollection.innerHTML = "<ul></ul>"
+        stockCollection.append(stockDiv)
 
+    }
+
+    function setStockImage(stockSymbol){
+      fetch(`http://localhost:3000/api/v1/stocks/${stockSymbol}`)
+       .then(response => response.json())
+       .then(data => {
+         searchImg.setAttribute("src", `${data.url}`)
+       })
+    }
+
+  function getStocks(){fetch(stocksUrl)
+    .then(response => response.json())
+    .then(data => {
+        renderStocks(data)
+    })
+  }
+
+    function renderStocks(stocks){
+      stocks.forEach(stock =>{
+                  renderStock(stock)
+              })
+    }
+
+    function renderStock(stock){
+      const stockDiv = document.createElement('div')
+      const stockImg = document.createElement('img')
+
+      // stockNewsUl.className = "stock-news"
+
+    stockImg.src = stock.logo_url
+    stockImg.setAttribute("class", "company_logo")
     stockDiv.innerHTML = `
-    <h1>${stock.quote.companyName}</h1><br>
-    <b>avg total volume:</b> ${stock.quote.avgTotalVolume}<br>
-    <b>change percent:</b> ${stock.quote.changePercent}<br>
-    <b>latest price:</b> ${stock.quote.latestPrice}<br>
-    <b>latest update:</b> ${stock.quote.latestTime}<br>
-    <b>market cap:</b> ${stock.quote.marketCap}<br>
-    <b>pe ratio:</b> ${stock.quote.peRatio}<br>
-    <b>primary exchange:</b> ${stock.quote.primaryExchange}<br>
-    <b>symbol:</b> ${stock.quote.symbol}<br>
-    <b>week52High:</b> ${stock.quote.week52High}<br>
-    <b>week52Low:</b> ${stock.quote.week52Low}<br>
-    <b>ytd change:</b> ${stock.quote.ytdChange}<br>
-    <b>news:</b><br>
+    <h1>${stock.company_name}</h1><br>
+    <button class="show-info">Show Info</button>
     `
-    stockDiv.prepend(searchImg)
-    stockCollection.innerHTML = "<ul></ul>"
-    stockCollection.append(stockDiv)
+    stockDiv.className = "each-stock"
+    stockDiv.dataset.avg_total_volume = `${stock.avg_total_volume}`
+    stockDiv.dataset.change_percent = `${stock.change_percent}`
+    stockDiv.dataset.latest_price = `${stock.latest_price}`
+    stockDiv.dataset.latest_update = `${stock.latest_update}`
+    stockDiv.dataset.market_cap = `${stock.market_cap}`
+    stockDiv.dataset.pe_ratio = `${stock.pe_ratio}`
+    stockDiv.dataset.primary_exchange = `${stock.primary_exchange}`
+    stockDiv.dataset.symbol = `${stock.symbol}`
+    stockDiv.dataset.ytd_change = `${stock.ytd_change}`
+    stockDiv.dataset.news = `${stock.news}`
 
-}
-
-function setStockImage(stockSymbol){
-  fetch(`http://localhost:3000/api/v1/stocks/${stockSymbol}`)
-   .then(response => response.json())
-   .then(data => {
-     searchImg.setAttribute("src", `${data.url}`)
-   })
-}
-function getStocks(){
-   fetch(stocksUrl)
-  .then(response => response.json())
-  .then(data => {
-  renderStocks(data)
- })
-}
-
-function renderStocks(stocks){
-  stocks.forEach(stock =>{
-  renderStock(stock)
- })
-}
-
-function renderStock(stock){
-  let stockDiv = document.createElement('div')
-  let stockImg = document.createElement('img')
-  let stockNewsUl = document.createElement('ul')
-
-  stockImg.src = stock.logo_url
-  stockImg.setAttribute("class", "company_logo")
-  stockDiv.innerHTML = `
-  <h1>${stock.company_name}</h1><br>
-  <b>avg total volume:</b> ${stock.avg_total_volume}<br>
-  <b>change percent:</b> ${stock.change_percent}<br>
-  <b>latest price:</b> ${stock.latest_price}<br>
-  <b>latest update:</b> ${stock.latest_update}<br>
-  <b>market cap:</b> ${stock.market_cap}<br>
-  <b>pe ratio:</b> ${stock.pe_ratio}<br>
-  <b>primary exchange:</b> ${stock.primary_exchange}<br>
-  <b>symbol:</b> ${stock.symbol}<br>
-  <b>ytd change:</b> ${stock.ytd_change}<br>
-  <b>news:</b><br>
-  `
-  stockDiv.prepend(stockImg)
-
-  newsArray = stock.news.split("#<IEX::Resources::News ")
-  newsArray.shift()
-  // console.log(newsArray)
-
-  newsArray.forEach(i => {
-
-  i.slice(1, -3)
-
-  let keyVal = i.split("=");
-  console.log(keyVal)
-  obj = { }
-  obj["date"] = keyVal[1].slice(0, -9)
-  obj["headline"] = keyVal[2].slice(1, -1)
-  obj["body"] = keyVal[5].slice(1, -5)
-  obj["src"] = keyVal[6].slice(1, -8)
+      stockDiv.prepend(stockImg)
 
 
-  li = document.createElement('li')
-  li.innerHTML =
-  `<h3>${obj.headline}</h3><br>
-  <p>${obj.body}<br>source: ${obj.src}</p><br>
-  `
-  stockNewsUl.append(li)
 
-})
-
-stockDiv.append(stockNewsUl)
-stockCollection.append(stockDiv)
+        // stockDiv.append(stockNewsUl)
+        stockCollection.append(stockDiv)
 
 
-}
+    }
 
 
 
